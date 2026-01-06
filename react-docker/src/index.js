4f5bde39bb6760be4bb84a10606543a9d48cfad2
divulgacheur <git@theopeltier.me>
2026-01-06 21:06:54 +0100

    :boom: big jump with new panels, dynamic background image, leaflet map...

--- react-docker/src/index.js
+++ react-docker/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'leaflet/dist/leaflet.css';
 
 import TrainPosition from './TrainPosition';
 import reportWebVitals from './reportWebVitals';
